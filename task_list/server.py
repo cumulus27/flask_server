09bed89aae285255b0edecbd42a0704511f7b1bb
Py Zhao <zhaopy4721@outlook.com>
2019-03-16 15:34:04 +0800

    Add heroku support.

--- task_list/server.py
+++ task_list/server.py
@@ -1,4 +1,3 @@
-
 """
     Flask server.
     export FLASK_APP=server.py
@@ -8,7 +7,14 @@
 from flask import Flask
 from flask import jsonify
 
-app = Flask(__name__)
+from flask import (
+    Blueprint, flash, redirect, render_template, request, url_for
+)
+
+from task_list import db
+from task_list.models import Task
+
+app = Blueprint('task_list', __name__)
 
 
 @app.route('/v1/test')
