829b008a92bfbc730733f460c2ef2c040fac8c7a
Py Zhao <zhaopy4721@outlook.com>
2019-03-19 16:43:20 +0800

    Totally copy a project.

---
+++ boot.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+source /home/zte/anaconda3/bin/activate flask_server
+
+while true; do
+    flask deploy
+    if [[ "$?" == "0" ]]; then
+        break
+    fi
+    echo Deploy command failed, retrying in 5 secs...
+    sleep 5
+done
+
+exec gunicorn -b :5000 --access-logfile - --error-logfile - flask_server:app
--- flask_server.py
+++ flask_server.py
@@ -15,7 +15,7 @@ import sys
 import click
 from flask_migrate import Migrate, upgrade
 from app import create_app, db
-# from app.models import User, Follow, Role, Permission, Post, Comment
+from app.models import User, Follow, Role, Permission, Post, Comment
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 migrate = Migrate(app, db)
@@ -23,9 +23,8 @@ migrate = Migrate(app, db)
 
 @app.shell_context_processor
 def make_shell_context():
-    # return dict(db=db, User=User, Follow=Follow, Role=Role,
-    #             Permission=Permission, Post=Post, Comment=Comment)
-    return dict(db=db)
+    return dict(db=db, User=User, Follow=Follow, Role=Role,
+                Permission=Permission, Post=Post, Comment=Comment)
 
 
 @app.cli.command()
@@ -51,3 +50,29 @@ def test(coverage):
         COV.html_report(directory=covdir)
         print('HTML version: file://%s/index.html' % covdir)
         COV.erase()
+
+
+@app.cli.command()
+@click.option('--length', default=25,
+              help='Number of functions to include in the profiler report.')
+@click.option('--profile-dir', default=None,
+              help='Directory where profiler data files are saved.')
+def profile(length, profile_dir):
+    """Start the application under the code profiler."""
+    from werkzeug.contrib.profiler import ProfilerMiddleware
+    app.wsgi_app = ProfilerMiddleware(app.wsgi_app, restrictions=[length],
+                                      profile_dir=profile_dir)
+    app.run()
+
+
+@app.cli.command()
+def deploy():
+    """Run deployment tasks."""
+    # migrate database to latest revision
+    upgrade()
+
+    # create or update user roles
+    Role.insert_roles()
+
+    # ensure all users are following themselves
+    User.add_self_follows()
